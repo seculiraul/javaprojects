5a35bb7b1dc50c2380eb45e664efe2532e3b0d12
seculiraul <71821279+seculiraul@users.noreply.github.com>
2021-02-25 19:47:24 +0200

    Abstract Classes, Interfaces, Inner

---
+++ Abstract Classes, Interfaces, Inner/Abstract Classes Animals/src/com/timbuchalka/CanFly.java
@@ -0,0 +1,4 @@
+package com.timbuchalka; 
+public interface CanFly {
+    void fly();
+}
---
+++ Abstract Classes, Interfaces, Inner/Abstract+LinkedLists/src/com/timbuchalka/SearchTree.java
@@ -0,0 +1,126 @@
+package com.timbuchalka;
+
+
+
+public class SearchTree implements NodeList {
+
+    private ListItem root = null;
+
+    public SearchTree(ListItem root) {
+        this.root = root;
+    }
+
+    @Override
+    public ListItem getRoot() {
+        return this.root;
+    }
+
+    @Override
+    public boolean addItem(ListItem newItem) {
+        if (this.root == null) {
+            // the tree was empty, so our item becomes the head of the tree
+            this.root = newItem;
+            return true;
+        }
+
+        // otherwise, start comparing from the head of the tree
+        ListItem currentItem = this.root;
+        while (currentItem != null) {
+            int comparison = (currentItem.compareTo(newItem));
+            if (comparison < 0) {
+                // newItem is greater, move right if possible
+                if (currentItem.next() != null) {
+                    currentItem = currentItem.next();
+                } else {
+                    // there's no node to the right, so add at this point
+                    currentItem.setNext(newItem);
+                    return true;
+                }
+            } else if (comparison > 0) {
+                // newItem is less, move left if possible
+                if (currentItem.previous() != null) {
+                    currentItem = currentItem.previous();
+                } else {
+                    // there's no node to the left, so add at this point
+                    currentItem.setPrevious(newItem);
+                    return true;
+                }
+            } else {
+                // equal, so don't add
+                System.out.println(newItem.getValue() + " is already present");
+                return false;
+            }
+        }
+        // we can't actually get here, but Java complains if there's no return
+        return false;
+    }
+
+    @Override
+    public boolean removeItem(ListItem item) {
+        if (item != null) {
+            System.out.println("Deleting item " + item.getValue());
+        }
+        ListItem currentItem = this.root;
+        ListItem parentItem = currentItem;
+
+        while (currentItem != null) {
+            int comparison = (currentItem.compareTo(item));
+            if (comparison < 0) {
+                parentItem = currentItem;
+                currentItem = currentItem.next();
+            } else if (comparison > 0) {
+                parentItem = currentItem;
+                currentItem = currentItem.previous();
+            } else {
+                // equal: we've found the item so remove it
+                performRemoval(currentItem, parentItem);
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    private void performRemoval(ListItem item, ListItem parent) {
+        if (item.next() == null) {
+            if (parent.next() == item) {
+                parent.setNext(item.previous());
+            } else if (parent.previous() == item) {
+                parent.setPrevious(item.previous());
+            } else {
+                this.root = item.previous();
+            }
+        } else if (item.previous() == null) {
+            if (parent.next() == item) {
+                parent.setNext(item.next());
+            } else if (parent.previous() == item) {
+                parent.setPrevious(item.next());
+            } else {
+                this.root = item.next();
+            }
+        } else {
+            ListItem current = item.next();
+            ListItem leftmostParent = item;
+            while (current.previous() != null) {
+                leftmostParent = current;
+                current = current.previous();
+            }
+            item.setValue(current.getValue());
+            if (leftmostParent == item) {
+                item.setNext(current.next());
+            } else {
+                leftmostParent.setPrevious(current.next());
+            }
+        }
+    }
+
+    @Override
+    public void traverse(ListItem root) {
+        if (root != null) {
+            traverse(root.previous());
+            System.out.println(root.getValue());
+            traverse(root.next());
+        }
+
+    }
+}
